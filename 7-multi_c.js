33ff11c394660eb41f719315696c2cce86cb2d50
verb <vanbrght@gmail.com>
2025-07-14 20:12:55 +0000

    Fix Task 7: Correct loop and output

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -3,9 +3,7 @@ const arg = parseInt(process.argv[2]);
 if (isNaN(arg)) {
   console.log("Missing number of occurrences");
 } else {
-  let output = "";
   for (let i = 0; i < arg; i++) {
-    output += "C is fun\n";
+    console.log("C is fun");
   }
-  console.log(output.trim());
 }
