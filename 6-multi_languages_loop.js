10ab7e4a757b2b7135238f978b395b84ed068ae8
verb <vanbrght@gmail.com>
2025-07-14 20:02:53 +0000

    Add Task 6: Loop to languages

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
+let output = "";
+
+for (let i = 0; i < lines.length; i++) {
+  output += lines[i] + "\n";
+}
+
+console.log(output.trim());
